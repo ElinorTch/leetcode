0f43fa72c152d45cf82cef8db12c01f7a6e0f085
Elinor Tchamo <tchamoelii@gmail.com>
2025-12-14 12:46:01 +0100

    Feat: minimum operations to make array sum divisible by k

---
+++ minimum-operations-to-make-array-sum-divisible-by-k.py
@@ -0,0 +1,12 @@
+def minOperations(nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: int
+    """
+    total = sum(nums)
+    near_multiple = int(total / k) * k
+    return total - near_multiple
+    
+if __name__ == "__main__":
+    print(minOperations([3, 9, 7], 5))
