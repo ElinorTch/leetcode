58b4f2e033922abd881421ac643931c9a4333203
Elinor Tchamo <tchamoelii@gmail.com>
2025-12-15 20:32:08 +0100

    Feat: maximum-substrings-with-distinct-start

---
+++ maximum-substrings-with-distinct-start.py
@@ -0,0 +1,18 @@
+def maxDistinct(s: str):
+    """
+    :type s: str
+    :rtype: int
+    """
+    str_dict: dict = {}
+    count: int = 0
+    for value in s:
+        if value not in str_dict:
+            str_dict[value] = 1
+            count = count + 1
+    return count
+    # return len(set(s))
+            
+    
+if __name__ == "__main__":
+    print(maxDistinct("aaaa"))
+    
